2e6d26ac2c4e18e5860b376b32a6d474ffa0b7b6
lunahobi <muradyan.lusine.05@mail.ru>
2023-04-28 10:30:01 +0300

    Исправила Alt+Tab v3

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,59 @@
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-//        Pattern run = Pattern.compile("Run" + ".+");
-//        Pattern alt_tab = Pattern.compile("Alt\\+Tab" + ".*");
-//        Pattern alt_del = Pattern.compile("Alt\\+Delete" + ".*");
         L1List l = new L1List();
         Scanner in = new Scanner(System.in);
-        while (in.hasNextLine()){
+        int count = 0;
+        while (in.hasNextLine()) {
             String s = in.nextLine();
             if (s.equals("end")) break;
-            if (s.startsWith("Run")){
+            if (s.startsWith("Run")) {
                 String p = s.replaceAll("Run", "").trim();
                 l.insert(p);
-            }
-            else if (s.startsWith("Alt")){
-                String[] m = s.split("\\+");
-                for (int i = 0; i < m.length; i++){
-                    if (m[i].equals("Delete")){
+                count++;
+            } else if (s.startsWith("Alt")) {
+                if (s.split("\\+")[1].equals("Tab")) {
+                    int c = s.split("\\+").length - 1;
+                    for (int k = 0; k < c; k++) {
                         try {
-                            l.erase();
-                        }catch (Exception e){
+                            l.forward();
+                        } catch (Exception e) {
                             l.toFront();
                         }
                     }
-                    else if (m[i].equals("Tab")){
-                        int c = 0;
-                        for (int j = 0; j < m.length; j++) {
-                            if (m[i].equals("Tab")) {
-                                c++;
-                            }
-                        }
-                        for (int j = 0; j < c; j++){
+                    String p = l.after();
+                    l.erase();
+                    l.toFront();
+                    for (int i = 0; i < count; i++) {
+                        for (int j = 0; j < (count - i); j++) {
                             try {
                                 l.forward();
-                            }catch (Exception e){
-                                l.toFront();
-                            }
-                            try {
-                                String p = l.after();
-                                l.erase();
-                                l.insert(p);
-                            }catch (Exception e){
+                            } catch (Exception ex) {
                                 l.toFront();
                             }
                         }
+                        try {
+                            l.after();
+                        } catch (Exception ex) {
+                            l.toFront();
+                        }
+                        s = l.after();
+                        l.erase();
+                        l.insert(s);
+                        l.toFront();
+                    }
+                    l.insert(p);
+                }
+                else if (s.split("\\+")[1].equals("Delete")) {
+                    int c = s.split("\\+").length - 1;
+                    for (int i = 0; i < c; i++) {
+                        try {
+                            l.erase();
+                        } catch (Exception e) {;}
                     }
                 }
-            }
-            else{
+            } else {
                 System.out.println("Введена неправильная команда");
                 continue;
             }
